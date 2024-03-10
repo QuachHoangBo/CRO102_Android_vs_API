63eafc78ea6662c1cec951ac19717419cf80a926
QuachHoangBo <121910981+QuachHoangBo@users.noreply.github.com>
2024-03-10 22:49:49 +0700

    studying
    studying

--- app/src/main/java/fpoly/edu/boqhpd0840/lab1/Login.java
+++ app/src/main/java/fpoly/edu/boqhpd0840/lab1/Login.java
@@ -3,18 +3,14 @@ package fpoly.edu.boqhpd0840.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import fpoly.edu.boqhpd0840.R;
 
-public class FireBase extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
-    Button googleAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fire_base);
-
-        googleAuth = findViewById(R.id.btnloginEmail);
+        setContentView(R.layout.activity_login);
     }
 }
