30e7462acd6e34a28d0efcb7ebb0e15c8e228438
QuachHoangBo <121910981+QuachHoangBo@users.noreply.github.com>
2024-03-12 13:54:40 +0700

    studying

--- app/src/main/java/fpoly/edu/boqhpd0840/lab1/FireBase.java
+++ app/src/main/java/fpoly/edu/boqhpd0840/lab1/FireBase.java
@@ -2,19 +2,30 @@ package fpoly.edu.boqhpd0840.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import fpoly.edu.boqhpd0840.R;
 
 public class FireBase extends AppCompatActivity {
 
-    Button googleAuth;
+    Button googleAuth,phoneAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_base);
 
-        googleAuth = findViewById(R.id.btnloginEmail);
+        googleAuth = findViewById(R.id.btnloginWithEmail);
+        phoneAuth = findViewById(R.id.btnloginWithPhoneNumer);
+
+        phoneAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FireBase.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/fpoly/edu/boqhpd0840/lab1/SignUp.java
+++ app/src/main/java/fpoly/edu/boqhpd0840/lab1/SignUp.java
@@ -2,15 +2,41 @@ package fpoly.edu.boqhpd0840.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import fpoly.edu.boqhpd0840.R;
 
 public class SignUp extends AppCompatActivity {
 
+    Button btnGoLogin,goBack;
+    EditText edtEmail,edtPassword,edtPassword2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        goBack = findViewById(R.id.btnGoback);
+        edtEmail = findViewById(R.id.edtEmailSighUp);
+        edtPassword = findViewById(R.id.edtPassSignUp);
+        edtPassword2 = findViewById(R.id.edtPassSignUp2);
+
+        btnGoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
